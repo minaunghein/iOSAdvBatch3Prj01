48a41c40060294a4c5a064d70d537391114ab29d
Min Aung Hein <minaunghein@gmail.com>
2018-02-11 16:15:24 +0630

    Initial Commit

---
+++ bigproject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bigproject
+//
+//  Created by Min Aung Hein on 2/11/18.
+//  Copyright © 2018 Min Aung Hein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
